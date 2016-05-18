108e4b703ef40240808764c390e9cc8fa627c1bd
Jimmy Charnley Kromann <jimmy@charnley.dk>
2016-05-18 11:16:05 +0200

    added new font

---
+++ setup_font_mononoki.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir mononoki
+cd mononoki
+
+wget -O mononoki.zip https://github.com/madmalik/mononoki/blob/master/export/mononoki.zip?raw=true
+
+unzip mononoki.zip
+
+cp *.ttf ~/.fonts
+
+cd ..
+# rm -rf mononoki
+
+fc-cache -vf ~/.fonts
